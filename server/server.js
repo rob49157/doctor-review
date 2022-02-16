19937ce827d4dbc93db6a9bd6fb36be175549b71
Roberto Buzeta <robertopaipo@hotmail.com>
2022-02-16 16:23:38 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client")));
+  app.use(express.static(path.join(__dirname, "../client.build")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
